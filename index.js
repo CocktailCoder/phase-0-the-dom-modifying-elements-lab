b8b0eb76b3febe8391c25ac627fa0120fddd3522
cocktailcoder <iokreddit@protonmail.com>
2021-06-06 14:23:07 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Write your code here!
+/*document.body.appendChild(element); 
+const ul = document.createElement('ul');
+
+for (let i = 0; i < 3; i++) {
+  const li = document.createElement('li');
+  li.innerHTML = (i + 1).toString();
+  ul.appendChild(li);
+}
+
+element.appendChild(ul); */
+
+document.body.remove('main');
+
+const newHeader = document.createElement('h1')
+newHeader.setAttribute('id','victory');
+//document.getElementById('h1').innerHTML = "Sean is the champion";
+newHeader.innerHTML = "Sean is the champion";
